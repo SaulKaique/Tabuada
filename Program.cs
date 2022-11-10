5b30e2136b512669ee985cf53a4ee59076814021
SaulKaique <Saul.silva@etec.sp.gov.br>
2022-11-10 14:46:34 -0300

    completo ja testado

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("......Tabuada do número.....:");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int vezes = 0;
+
+int resultado;
+
+while ( vezes <= 10 )
+{
+    resultado = numero * vezes ;
+
+    Console.Write($"{numero} X {vezes} = {resultado}\n");
+    vezes += 1;
+};
+
+
